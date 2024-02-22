b7703760e80c901c3233196ef61ee0ef79a6f088
Sascha Witte <witte@geoplex.de>
2024-02-22 15:26:26 +0100

    some changes on the getter methods (might be removed later on)

--- PlexMapViewerAddon/store/gettersPlexMapViewer.js
+++ PlexMapViewerAddon/store/gettersPlexMapViewer.js
@@ -1,21 +1,11 @@
+import plexmapState from "./statePlexmap";
 
-
-import {generateSimpleGetters} from "../../../../app-store/utils/generators";
-import state from "./statePlexMapViewer";
-
-const getters = {
-    /**
-     * Returns an object of simple getters for a state object, where
-     * simple means that they will just return an entry for any key.
-     * For example, given a state object {key: value}, an object
-     * {key: state => state[key]} will be returned.
-     * This is useful to avoid writing basic operations.
-     * @param {object} state state to generate getters for
-     * @returns {object.<string, function>} object of getters
-     */
-    ...generateSimpleGetters(state)
-
-    // NOTE overwrite (or create additional) getters here if you need special behavior in them
+const plexmapGetters = {
+    all     : plexmapState => plexmapState,
+    inital  : plexmapState => plexmapState.inital,
+    plexmap : plexmapState => plexmapState.plexmap,
+    viewer  : plexmapState => plexmapState.viewer,
+    map     : plexmapState => plexmapState.map,
 };
 
-export default getters;
+export default plexmapGetters
